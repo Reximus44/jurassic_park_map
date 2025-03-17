b59a8ae5cdbd8b337e6d8d78c91f96cb78343304
Reximus44 <Dylancokley8@gmail.com>
2025-03-17 01:58:56 -0700

    add clarification in some comments

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./components/App/App";
 import { HashRouter } from "react-router-dom";
 
-// BrowserRouter was not working so HashRouter was used instead 
+// BrowserRouter was not working with GH Pages so HashRouter was used instead 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <HashRouter>
